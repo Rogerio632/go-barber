c548176786a0245c3ea3427c98e21fd3347f4493
rogerio632 <rogeriofilho632@gmail.com>
2019-10-26 19:15:17 -0300

    update repo with new knowledges learned today

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/post/:id', (req, res) => {
-  const { id } = req.params;
-
-  return res.send(`O número digitado foi: ${id}`);
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rogério De Oliveira',
+    email: 'rogeriofilho632@gmail.com',
+    password_hash: '12344321',
+  });
+  return res.json(user);
 });
-
 
 export default routes;

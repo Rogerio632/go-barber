44012931eeeb926444af9886e0f613b673602a50
Rogério <rogeriofilho632@gmail.com>
2019-11-26 13:51:17 -0300

    add: index method to AppointmentController

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,6 +4,29 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 
 class AppointmentController {
+  async index(req, res) {
+    const showAppointment = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name', 'email'],
+        },
+      ],
+    });
+    return res.json(showAppointment);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -13,7 +36,7 @@ class AppointmentController {
     if (!(await schema.isValid(req.body))) {
       return res
         .status(400)
-        .json({ error: "I think you've written something wrong!" });
+        .json({ error: "Maybe you've written something wrong!" });
     }
 
     const { provider_id, date } = req.body;
@@ -28,32 +51,38 @@ class AppointmentController {
       });
     }
 
-    /* Checa se há data anterior a data atual */
+    /* INÍCIO */
+
+    /* Recebe a data com timezone, transforma no formato do new Date() e arredonda o horário */
     const hourStart = startOfHour(parseISO(date));
 
+    /* Checa se há data anterior a data atual */
     if (isBefore(hourStart, new Date())) {
       return res.status(400).json({ error: 'past date   are not permitted' });
     }
+    /* FIM */
+
     /* Verifica se a data está preenchida */
     const checkAvailable = await Appointment.findOne({
       where: {
         provider_id,
         canceled_at: null,
+        date: hourStart,
       },
     });
 
-    /*   if (checkAvailable) {
-      return res.status(400).json({ error: 'Appointment date is not avaible' });
-    } */
-    return res.json(checkAvailable);
-    /*    const store = await Appointment.create({
+    if (checkAvailable) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+    const store = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.status(201).json(store);
-  } */
   }
 }
 

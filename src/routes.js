467ab52d97c40af63edb377c30303370b8d83b90
Rogério De Oliveira <rogeriofilho632@gmail.com>
2019-11-27 08:23:12 -0300

    add: Schedule index method to routes

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,11 +21,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
-
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;

f8ed971fe512f4054c4118e2987c9cf5b1f126d7
Rogério Filho <rogeriofilho632@gmail.com>
2019-12-03 17:53:20 -0300

    feat: new appointments 'n notification methods

--- src/routes.js
+++ src/routes.js
@@ -24,10 +24,12 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 
-routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.get('/schedule', ScheduleController.index);
 

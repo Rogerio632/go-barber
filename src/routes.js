4c380a587f637b8473c7f91391cd95c2ef9883f3
Rogério Filho <rogeriofilho632@gmail.com>
2019-12-06 08:45:51 -0300

    fix: /providers route

--- src/routes.js
+++ src/routes.js
@@ -23,8 +23,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
-routes.get('/providers', AppointmentController.index);
-routes.get('/providers/:providerId/available', AvailableController.index );
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
